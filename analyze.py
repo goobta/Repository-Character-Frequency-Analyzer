de1a5750059d1cf344d79a0d58a1e308f7019356
Ankur Gupta <rukna1000@gmail.com>
2018-06-15 12:11:22 -0400

    Got the basic version working... need to do some cleaning up

--- analyze.py
+++ analyze.py
@@ -1,7 +1,9 @@
 # https://gitlab.com/agupta628
 # This script will find determine the char frequency in your projects
 
+from multiprocessing.dummy import Pool as ThreadPool
 from subprocess import call
+import operator
 import requests
 import glob
 
@@ -11,16 +13,45 @@ api_url_gitlab = "https://gitlab.com/api/v4/users/" + username + "/projects"
 
 char_freq_dict = {}
 
+excluded_files = [".git",
+                  ".log",
+                  ".pyc"]
+
 def get_char_count(url):
-    # call(["git", "clone", url])
-    
+    call(["git", "clone", url])
     dir_name = url.split("/")[-1][:-4]
-    print(dir_name)
+    
+    for f in glob.iglob(dir_name + "/**/*.*", recursive=True):
+        cont = True
+        for e in excluded_files:
+            if e in f:
+                cont = False
 
+        if not cont:
+            continue
 
+        buff = open(f,"rb").read()
+        
+        for char in buff:
+            if char == " ":
+                char = "space"
+            elif char == "\n":
+                char = "enter"
+            elif char == "\t":
+                char = "tab"
 
-repos = requests.get(api_url_gitlab)
+            try:
+                char_freq_dict[char] += 1
+            except:
+                char_freq_dict[char] = 1
 
-for repo in repos.json():
-    get_char_count(repo['http_url_to_repo'])
+    call(["rm", "-rf", dir_name])
 
+res = requests.get(api_url_gitlab).json()
+repos = [r['http_url_to_repo'] for r in res]
+
+pool = ThreadPool(thread_count)
+pool.map(get_char_count, repos)
+
+for key, value in sorted(char_freq_dict.items(), key=operator.itemgetter(1), reverse=True):
+    print("Character {} had {} hits".format(chr(key), value))
